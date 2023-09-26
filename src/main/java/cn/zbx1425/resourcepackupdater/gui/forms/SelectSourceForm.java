5479522595a2d5cd30272d5e474ac33f66bcda5a
zbx1425 <zbx1425@outlook.com>
2023-09-26 23:38:09 +0800

    Add SelectSourceForm

---
+++ src/main/java/cn/zbx1425/resourcepackupdater/gui/forms/SelectSourceForm.java
@@ -0,0 +1,105 @@
+package cn.zbx1425.resourcepackupdater.gui.forms;
+
+import cn.zbx1425.resourcepackupdater.ResourcePackUpdater;
+import cn.zbx1425.resourcepackupdater.gui.GlHelper;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.Minecraft;
+
+import java.io.IOException;
+
+public class SelectSourceForm implements GlScreenForm {
+
+    public float selectSourceFormWidth = 500, selectSourceFormHeight = 400;
+
+    int selectedIndex = -1;
+
+    @Override
+    public void render() {
+        int sourceSize = ResourcePackUpdater.CONFIG.sourceList.value.size();
+        selectSourceFormHeight = 30 + 30 + 30 + sourceSize * 40 + (sourceSize - 1) * 10;
+        if (selectedIndex == -1) {
+            selectedIndex = ResourcePackUpdater.CONFIG.sourceList.value.indexOf(ResourcePackUpdater.CONFIG.activeSource.value);
+        }
+
+        GlHelper.setMatCenterForm(selectSourceFormWidth, selectSourceFormHeight, 0.6f);
+        GlHelper.begin(GlHelper.PRELOAD_FONT_TEXTURE);
+        GlScreenForm.drawShadowRect(selectSourceFormWidth, selectSourceFormHeight, 0xffdee6ea);
+
+        GlHelper.drawString(30, 30, selectSourceFormWidth - 60, 30, 20,
+                "Select Source", 0xff222222, false, false);
+        for (int i = 0; i < sourceSize; i++) {
+            if (i == selectedIndex) {
+                GlHelper.blit(30, 30 + 30 + i * 50, selectSourceFormWidth - 60, 40, 0xff63a0c6);
+            } else {
+                GlHelper.blit(30, 30 + 30 + i * 50, selectSourceFormWidth - 60, 40, 0xffc0d2db);
+            }
+            GlHelper.drawString(30 + 15, 30 + 30 + i * 50 + 10, selectSourceFormWidth - 90, 40, 20,
+                    ResourcePackUpdater.CONFIG.sourceList.value.get(i).name, 0xff222222, false, false);
+        }
+
+        GlHelper.end();
+    }
+
+    private int heldKey = -1;
+
+    @Override
+    public boolean shouldStopPausing() {
+        var glfwWindow = Minecraft.getInstance().getWindow().getWindow();
+        if (InputConstants.isKeyDown(glfwWindow, InputConstants.KEY_UP)) {
+            if (heldKey != InputConstants.KEY_UP) {
+                selectedIndex = Math.max(0, selectedIndex - 1);
+                heldKey = InputConstants.KEY_UP;
+            }
+        } else if (InputConstants.isKeyDown(glfwWindow, InputConstants.KEY_DOWN)) {
+            if (heldKey != InputConstants.KEY_DOWN) {
+                selectedIndex = Math.min(ResourcePackUpdater.CONFIG.sourceList.value.size() - 1, selectedIndex + 1);
+                heldKey = InputConstants.KEY_DOWN;
+            }
+        } else if (InputConstants.isKeyDown(glfwWindow, InputConstants.KEY_RETURN)) {
+            if (heldKey != InputConstants.KEY_RETURN) {
+                ResourcePackUpdater.CONFIG.activeSource.value = ResourcePackUpdater.CONFIG.sourceList.value.get(selectedIndex);
+                ResourcePackUpdater.CONFIG.activeSource.isFromLocal = true;
+                try {
+                    ResourcePackUpdater.CONFIG.save();
+                } catch (IOException ignored) { }
+                return true;
+            }
+        } else if (InputConstants.isKeyDown(glfwWindow, InputConstants.KEY_ESCAPE)) {
+            return true;
+        } else {
+            heldKey = -1;
+        }
+        return false;
+    }
+
+    @Override
+    public void reset() {
+        selectedIndex = -1;
+        heldKey = -1;
+    }
+
+    @Override
+    public void printLog(String line) throws GlHelper.MinecraftStoppingException {
+
+    }
+
+    @Override
+    public void amendLastLog(String postfix) throws GlHelper.MinecraftStoppingException {
+
+    }
+
+    @Override
+    public void setProgress(float primary, float secondary) throws GlHelper.MinecraftStoppingException {
+
+    }
+
+    @Override
+    public void setSecondaryProgress(float value, String textValue) throws GlHelper.MinecraftStoppingException {
+
+    }
+
+    @Override
+    public void setException(Exception exception) throws GlHelper.MinecraftStoppingException {
+
+    }
+}
